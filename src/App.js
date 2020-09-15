173a35520f78c698d07fa49f81bc8ae001ef55fb
Vlp90 <vladimir.leloup@gmail.com>
2020-09-15 10:57:00 +0200

    installed MUI and ICONS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+    <Header />
+      <h1>App</h1>
     </div>
   );
 }
