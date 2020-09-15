0cc4f51ca717cea5973a14d2716233b5308d4ffc
Vlp90 <vladimir.leloup@gmail.com>
2020-09-15 10:37:24 +0200

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0qqIs9WpX2mfh6jnHVqXEqqbPTgJ8uXc",
+    authDomain: "stripe-c7cb3.firebaseapp.com",
+    databaseURL: "https://stripe-c7cb3.firebaseio.com",
+    projectId: "stripe-c7cb3",
+    storageBucket: "stripe-c7cb3.appspot.com",
+    messagingSenderId: "922691880732",
+    appId: "1:922691880732:web:f1ff47fa60560ddf939cd9",
+    measurementId: "G-ZV97KFCBEC"
+  };
