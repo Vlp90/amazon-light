68e4c1dc737b680394cddfaa6b2773870e883bd6
Vlp90 <vladimir.leloup@gmail.com>
2020-09-16 10:41:36 +0200

    solved problem with email logout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Checkout.css";
-import Subtotal from './Subtotal'
-import CheckoutProduct from './CheckoutProduct'
-import {useStateValue} from './StateProvider'
-
+import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 
 function Checkout() {
-    const [{ basket, user }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -18,17 +17,24 @@ function Checkout() {
         />
 
         <div>
-        <h3 className="checkout__titleEmail"> Hello, {user.email}</h3>
+          <h3 className="checkout__titleEmail">
+            {" "}
+            Hello {!user ? "Guest" : user?.email}
+          </h3>
           <h2 className="checkout__title"> Your shopping Basket</h2>
-          {basket.map(item => (
-
-          <CheckoutProduct id={item.id} title={item.title} image={item.image} price={item.price} rating={item.rating} />
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
           ))}
-
         </div>
       </div>
       <div className="checkout__right">
-          <Subtotal />
+        <Subtotal />
       </div>
     </div>
   );
