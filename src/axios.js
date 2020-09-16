ddec9259f3b9d4343f71c9c59b945292b02ea488
Vlp90 <vladimir.leloup@gmail.com>
2020-09-16 16:27:23 +0200

    app done need to be deployed

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-    baseURL: "http://localhost:5001/stripe-c7cb3/us-central1/api"
-//  baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
+    // baseURL: "http://localhost:5001/stripe-c7cb3/us-central1/api"
+ baseURL: 'https://us-central1-stripe-c7cb3.cloudfunctions.net/api'
 
 });
 
